e9c313db009402cfbd0fc2f6a5f919421b6fce2d
exeisnt <guliyevyashar03@gmail.com>
2024-12-27 03:07:01 +0400

    Update importlibraries.py

--- code/importlibraries.py
+++ code/importlibraries.py
@@ -1,1 +1,9 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
 
