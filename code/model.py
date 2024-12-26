0f61ce86193bb28b35ad369660ce89a19685bbe2
exeisnt <guliyevyashar03@gmail.com>
2024-12-27 03:17:29 +0400

    Update model.py

--- code/model.py
+++ code/model.py
@@ -1,1 +1,18 @@
+# Build CNN Model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
