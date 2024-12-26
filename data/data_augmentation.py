cf3d6c927d0d61d7397a75ec92a08341473a4ec8
exeisnt <guliyevyashar03@gmail.com>
2024-12-27 03:16:25 +0400

    Update data_augmentation.py

--- data/data_augmentation.py
+++ data/data_augmentation.py
@@ -1,1 +1,8 @@
-
+# Data Augmentation
+datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True
+)
+datagen.fit(X_train)
